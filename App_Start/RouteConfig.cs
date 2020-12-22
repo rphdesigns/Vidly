f4be813ab195df0aa34776bc025e9f408f820c0f
Michael Heesch <development@rphdesigns.com>
2020-12-22 15:02:25 -0500

    12/22/2020-Daily Changes

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -13,6 +13,17 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
+            //custome route
+            //has parameters for year and month
+            //custome routes must come before Default route
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate", 
+            //    "movies/released/{year}/{month}", 
+            //    new { Controller = "Movies", action = "ByReleaseDate"},
+            //    new { year = @"2015|2016", month = @"\d{2}" });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
